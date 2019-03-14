225a0e3ba2ee4106073202dc4a47188deac64eca
ManuelBeschi <manuel.beschi@gmail.com>
2019-03-14 14:05:48 +0100

    qp solver added

--- eigen_matrix_utils/include/eigen_matrix_utils/eigen_matrix_utils.h
+++ eigen_matrix_utils/include/eigen_matrix_utils/eigen_matrix_utils.h
@@ -10,48 +10,48 @@
 
 namespace eigen_utils
 {
-  
-  double standard_deviation(const Eigen::Ref<Eigen::VectorXd>& vet);
-  double correlation(const Eigen::Ref<Eigen::VectorXd>& vet1, const Eigen::Ref<Eigen::VectorXd>& vet2);
-  
-  inline bool getParam( const ros::NodeHandle& nh,  const std::string& key, Eigen::MatrixXd& matrix)
-  {
-    std::vector<std::vector<double>> mtx;
-    if (!rosparam_utilities::getParamMatrix<double>(nh, key, mtx))
-      return false;
-    
-    int nrows, ncols;
-    nrows = mtx.size();
-    if (nrows>0)
-      ncols = mtx.at(0).size();
-    else
-      ncols = 0;
-    
-    matrix.resize(nrows, ncols);
-    for (int iR = 0;iR<nrows;iR++)
-      for (int iC = 0;iC<ncols;iC++)
-        matrix(iR, iC) = mtx.at(iR).at(iC);
-      
-      return true;
-  }
-  
-  inline bool getParam( const ros::NodeHandle& nh,  const std::string& key, Eigen::VectorXd& vector)
-  {
-    std::vector<double> vtc;
-    if (!nh.getParam(key,vtc))
-      return false;
-    
-    int nrows;
-    nrows = vtc.size();
-    
-    vector.resize(nrows);
-    for (int iR = 0;iR<nrows;iR++)
-      vector(iR) = vtc.at(iR);
-      
-    return true;
-  }
-  
-  
+
+double standard_deviation(const Eigen::Ref<Eigen::VectorXd>& vet);
+double correlation(const Eigen::Ref<Eigen::VectorXd>& vet1, const Eigen::Ref<Eigen::VectorXd>& vet2);
+
+inline bool getParam( const ros::NodeHandle& nh,  const std::string& key, Eigen::MatrixXd& matrix)
+{
+  std::vector<std::vector<double>> mtx;
+  if (!rosparam_utilities::getParamMatrix<double>(nh, key, mtx))
+    return false;
+
+  int nrows, ncols;
+  nrows = mtx.size();
+  if (nrows>0)
+    ncols = mtx.at(0).size();
+  else
+    ncols = 0;
+
+  matrix.resize(nrows, ncols);
+  for (int iR = 0;iR<nrows;iR++)
+    for (int iC = 0;iC<ncols;iC++)
+      matrix(iR, iC) = mtx.at(iR).at(iC);
+
+  return true;
+}
+
+inline bool getParam( const ros::NodeHandle& nh,  const std::string& key, Eigen::VectorXd& vector)
+{
+  std::vector<double> vtc;
+  if (!nh.getParam(key,vtc))
+    return false;
+
+  int nrows;
+  nrows = vtc.size();
+
+  vector.resize(nrows);
+  for (int iR = 0;iR<nrows;iR++)
+    vector(iR) = vtc.at(iR);
+
+  return true;
+}
+
+
 };
 
 #endif
