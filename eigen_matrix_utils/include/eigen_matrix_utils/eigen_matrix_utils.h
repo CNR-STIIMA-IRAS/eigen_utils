7a7e8ee6cbe22ad58b61e73028b0313bc4f501ee
enricovillagrossi <enrico.villagrossi@stiima.cnr.it>
2021-02-19 12:54:09 +0100

    added functions getParam and setParam for std::vector<Eigen::VectorXd>

--- eigen_matrix_utils/include/eigen_matrix_utils/eigen_matrix_utils.h
+++ eigen_matrix_utils/include/eigen_matrix_utils/eigen_matrix_utils.h
@@ -51,6 +51,50 @@ inline bool getParam( const ros::NodeHandle& nh,  const std::string& key, Eigen:
   return true;
 }
 
+inline bool getParam( const ros::NodeHandle& nh,  const std::string& key, std::vector<Eigen::VectorXd>& vector )
+{
+  std::vector<std::vector<double>> mtx;
+  if (!rosparam_utilities::getParamMatrix<double>(nh, key, mtx))
+    return false;
+
+  int nrows, ncols;
+  nrows = mtx.size();
+  if (nrows>0)
+    ncols = mtx.at(0).size();
+  else
+    ncols = 0;
+
+  vector.clear();
+  for (int iC=0; iC<ncols; iC++)
+  {
+    Eigen::VectorXd col(nrows);
+    for (int iR=0; iR<nrows; iR++)
+      col(iR) = mtx.at(iR).at(iC);
+
+    vector.push_back(col);
+  }
+
+  return true;
+}
+
+inline bool setParam( const ros::NodeHandle& nh,  const std::string& key, const std::vector<Eigen::VectorXd>& vector )
+{
+  XmlRpc::XmlRpcValue data;
+  int iCol = 0;
+  for ( auto itCol=vector.begin(); itCol!=vector.end(); itCol++ )
+  {
+    int iEl = 0;
+    XmlRpc::XmlRpcValue col;
+    for (auto iRow=0; iRow<(*itCol).size(); iRow++)
+      col[iEl++] = (*itCol)[iRow];
+     
+    data[iCol++] = (XmlRpc::XmlRpcValue)col;
+  }
+
+  nh.setParam( key, data );
+  return true;
+}
+
 
 };
 
